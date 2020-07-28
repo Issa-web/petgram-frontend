70a6d9d786c5d914557483d274e6a113989155d0
devdave0 <dbchung24@gmail.com>
2020-07-28 13:02:00 -0400

    added js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-BASE_URL = ''
+// BASE_URL = ''
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    let main = document.getElementById("main")
+    // let main = document.getElementById("main")
     // main.innerHTML = '';
-
+    // let login = document.getElementById('login-modal')
+    // let modalContent = login.querySelector('div.modal-content')
+    
+    createLogin();
 });
 
+
+// Creates the login 
+
+function createLogin(){
+
+
+};
