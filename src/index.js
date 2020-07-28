cecf8a84f514dea61dffe78e1edeb9085e52a4cf
devdave0 <dbchung24@gmail.com>
2020-07-28 16:48:52 -0400

    added login modal

--- src/index.js
+++ src/index.js
@@ -1,20 +1,69 @@
-// BASE_URL = ''
+BASE_URL = 'http://localhost:3000/'
 
+PETS_URL = BASE_URL + 'pets'
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    // let main = document.getElementById("main")
-    // main.innerHTML = '';
-    // let login = document.getElementById('login-modal')
-    // let modalContent = login.querySelector('div.modal-content')
-    
-    createLogin();
+
+    loginModal();
+
+
+
+    // renderPets(); 
 });
 
 
-// Creates the login 
+// Render Pets 
+const renderPets = () => {
+    // clear working space 
+    let workingSpace = document.getElementById("working-space") 
+    workingSpace.innerHTML = '';
 
-function createLogin(){
+    fetchPets();
+}
 
 
-};
+const fetchPets = () => {
+    fetch(PETS_URL)
+    .then(resp => resp.json())
+    .then(pets => {
+        console.log(pets)
+        pets.forEach(pet => {
+            renderPet(pet);
+        })
+    })
+}
+
+const renderPet = (pet) => {
+    let workingSpace = document.getElementById("working-space")
+    // workingSpace.innerHTML = ''
+
+    // create pet div card 
+    let petCard = document.createElement('div')
+    petCard.className = 'pet-card'
+    workingSpace.appendChild(petCard)
+
+
+    // name
+    let petName = document.createElement('div')
+    petName.innerText = `${pet.name} the ${pet.animal}`
+    petCard.appendChild(petName)
+
+    // picture
+    let imgDiv = document.createElement('div')
+    let petPic = document.createElement('img')
+    petPic.id = 'pet-pic'
+    petPic.src = pet.pic_url
+    imgDiv.appendChild(petPic)
+    petCard.appendChild(imgDiv)
+
+    // description 
+    let description = document.createElement('div')
+    description.innerText = pet.description 
+    petCard.appendChild(description)
+
+}
+
+
+
+// };
